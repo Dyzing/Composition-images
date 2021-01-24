45e27bba216c7882091c7ea26b59737af7ba962b
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2021-01-24 19:07:05 +0100

    Library jpeg

--- Composition-images/Composition-images/Composition-images.cpp
+++ Composition-images/Composition-images/Composition-images.cpp
@@ -2,19 +2,19 @@
 //
 
 #include <iostream>
+#include <jpeglib.h>
+#include <stdio.h>
 
-int main()
+FILE* stream, * stream2;
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    errno_t err;
+    err = fopen_s(&stream, "../Photos/part1.jpg", "r");
+    if (err == 0)
+    {
+        printf("The file 'crt_fopen_s.c' was opened\n");
+    }
+	//jpeg_stdio_dest(&cinfo, outfile);
+    return 0;
+
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.

a1c9f166fa66dcead3bf3f92163b77365ec8c5f7
Guillaume <guillaume.trem@outlook.fr>
2021-01-25 23:01:05 +0100

    ajout de difference d'image

--- Composition-images/Composition-images/corona.cpp
+++ Composition-images/Composition-images/corona.cpp
@@ -11,6 +11,7 @@ struct Pixels {
 
 };
 typedef unsigned char byte;
+
 Pixels** init(int hauteur, int largeur) {
 	Pixels** tab = new Pixels * [hauteur];
 	Pixels* tab2 = new Pixels[hauteur * largeur];
@@ -18,10 +19,9 @@ Pixels** init(int hauteur, int largeur) {
 		tab[i] = &tab2[i * largeur];
 
 		for (int j = 0; j < largeur; j++) {
-			tab[i][j] = { 0,0,0,255 };
+			tab[i][j] = { 0,0,255,255 };
 		}
 	}
-
 
 	return tab;
 }
@@ -45,9 +45,8 @@ void TabToPixels(Pixels** pix, corona::Image* img) {
 			*iter++;
 		}
 	}
-	
 }
-int main()
+int main(int argc, char* argv[])
 {
 	corona::Image* image = corona::OpenImage("../Photos/part1.jpg", corona::PF_R8G8B8A8);
 	if (!image) {
@@ -57,7 +56,8 @@ int main()
 	int width = image->getWidth();
 	int height = image->getHeight();
 	void* pixels = image->getPixels();
-	Pixels** tab = init(width, height);
+	Pixels** tab1 = init(width, height);
+	Pixels** tab2 = init(width, height);
 	// we're guaranteed that the first eight bits of every pixel is red,
 	// the next eight bits is green, and so on...
 	
@@ -71,10 +71,37 @@ int main()
 			//tab[i][j] = { red,green,blue,alpha };
 		}
 	}
-	
 
-	corona::Image*  imagecop =	corona::CloneImage(image);
-	TabToPixels(tab, imagecop);
-	corona::SaveImage("../Photos/coucou.jpg", corona::FileFormat::FF_PNG, imagecop);
+	corona::Image* imagecop2 = corona::CloneImage(image);
+	TabToPixels(tab2, imagecop2);
+
+	for (int i = 0; i < width; ++i) {
+		for (int j = 0; j < height; ++j) {
+			if (j == height / 2)
+			{
+				tab1[i][j] = { 255,0,0,255 };
+			}
+		}
+	}
+
+	corona::Image* imagecop1 = corona::CloneImage(image);
+	TabToPixels(tab1, imagecop1);
+
+	Pixels** tab3 = init(width, height);
+	for (int i = 0; i < width; ++i) {
+		for (int j = 0; j < height; ++j) {
+			tab3[i][j].red = tab2[i][j].red - tab1[i][j].red;
+			tab3[i][j].green = tab2[i][j].green - tab1[i][j].green;
+			tab3[i][j].blue = tab2[i][j].blue - tab1[i][j].blue;
+			//tab3[i][j].alpha = tab2[i][j].alpha - tab1[i][j].alpha;
+		}
+	}
+
+	corona::Image* imagecop3 = corona::CreateImage(width, height, corona::PF_R8G8B8A8);
+	TabToPixels(tab3, imagecop3);
+
+	corona::SaveImage("../Photos/imagecop1.jpg", corona::FileFormat::FF_PNG, imagecop1);
+	corona::SaveImage("../Photos/imagecop2.jpg", corona::FileFormat::FF_PNG, imagecop2);
+	corona::SaveImage("../Photos/imagecop3.jpg", corona::FileFormat::FF_PNG, imagecop3);
 
 }

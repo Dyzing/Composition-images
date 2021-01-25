7fcff625b47854fd2b57b846f2433a06f9c6a845
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2021-01-25 21:42:38 +0100

    Init tab + tabToPixels

--- Composition-images/Composition-images/corona.cpp
+++ Composition-images/Composition-images/corona.cpp
@@ -10,7 +10,7 @@ struct Pixels {
 	int alpha;
 
 };
-
+typedef unsigned char byte;
 Pixels** init(int hauteur, int largeur) {
 	Pixels** tab = new Pixels * [hauteur];
 	Pixels* tab2 = new Pixels[hauteur * largeur];
@@ -18,12 +18,34 @@ Pixels** init(int hauteur, int largeur) {
 		tab[i] = &tab2[i * largeur];
 
 		for (int j = 0; j < largeur; j++) {
-			tab[i][j] = { 0,0,0,0 };
+			tab[i][j] = { 0,0,0,255 };
 		}
 	}
 
 
 	return tab;
+}
+
+void TabToPixels(Pixels** pix, corona::Image* img) {
+	int width = img->getWidth();
+	int height = img->getHeight();
+	void* p = img->getPixels();
+	Pixels tmp;
+	byte* iter = (byte*)p;
+	for (int i = 0; i < width; ++i) {
+		for (int j = 0; j < height; ++j) {
+			tmp = pix[i][j];
+			*iter = tmp.red;
+			*iter++;
+			*iter = tmp.green;
+			*iter++;
+			*iter = tmp.blue;
+			*iter++;
+			*iter = tmp.alpha;
+			*iter++;
+		}
+	}
+	
 }
 int main()
 {
@@ -38,7 +60,7 @@ int main()
 	Pixels** tab = init(width, height);
 	// we're guaranteed that the first eight bits of every pixel is red,
 	// the next eight bits is green, and so on...
-	typedef unsigned char byte;
+	
 	byte* p = (byte*)pixels;
 	for (int i = 0; i < width; ++i) {
 		for (int j = 0; j < height; ++j) {
@@ -46,11 +68,13 @@ int main()
 			int green = *p++;
 			int blue = *p++;
 			int alpha = *p++;
-			tab[i][j] = { red,green,blue,alpha };
+			//tab[i][j] = { red,green,blue,alpha };
 		}
 	}
 	
+
 	corona::Image*  imagecop =	corona::CloneImage(image);
+	TabToPixels(tab, imagecop);
 	corona::SaveImage("../Photos/coucou.jpg", corona::FileFormat::FF_PNG, imagecop);
 
 }
